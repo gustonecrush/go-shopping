0079450ae2c27f0e73711073e6046ff4fcb4a61c
gustonecrush <codeboxezz@gmail.com>
2023-02-12 05:12:32 +0700

    created config db struct and use it in initialize func

--- app/server.go
+++ app/server.go
@@ -23,11 +23,19 @@ type AppConfig struct {
 	AppPort string
 }
 
-func (server *Server) Initialize(appConfig AppConfig) {
+type DBConfig struct {
+	DBHost     string
+	DBUser     string
+	DBPassword string
+	DBName     string
+	DBPort     string
+}
+
+func (server *Server) Initialize(appConfig AppConfig, dbConfig DBConfig) {
 	fmt.Println("Welcome to " + appConfig.AppName)
 
 	var err error
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", getEnv("DB_USERNAME", ""), getEnv("DB_PASSWORD", ""), getEnv("DB_HOST",  "localhost"), getEnv("DB_PORT", "3336"), getEnv("DB_NAME", ""))
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", dbConfig.DBUser, dbConfig.DBPassword, dbConfig.DBHost, dbConfig.DBPort, dbConfig.DBName)
 	server.DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
@@ -53,6 +61,7 @@ func getEnv(key, fallback string) string {
 func Run() {
 	var server    = Server{}
 	var appConfig = AppConfig{}
+	var dbConfig  = DBConfig{}
 
 	err := godotenv.Load()
 	if err != nil {
@@ -63,6 +72,12 @@ func Run() {
 	appConfig.AppEnv  = getEnv("APP_ENV", "development")
 	appConfig.AppPort = getEnv("APP_PORT", "9999")
 
-	server.Initialize(appConfig)
+	dbConfig.DBHost     = getEnv("DB_HOST", "localhost")
+	dbConfig.DBUser     = getEnv("DB_USERNAME", "")
+	dbConfig.DBPassword = getEnv("DB_PASSWORD", "")
+	dbConfig.DBPort     = getEnv("DB_PORT", "3336")
+	dbConfig.DBName     = getEnv("DB_NAME", "")
+
+	server.Initialize(appConfig, dbConfig)
 	server.Run(":" + appConfig.AppPort)
 }
