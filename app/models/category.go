cb2f992733da5034899f52a2c497a79162206b2d
gustonecrush <codeboxezz@gmail.com>
2023-02-13 01:39:38 +0700

    model category

---
+++ app/models/category.go
@@ -0,0 +1,21 @@
+package models
+
+
+import (
+	"time"
+
+	"github.com/shopspring/decimal"
+	"gorm.io/gorm"
+)
+
+type Category struct {
+	ID 		     string `gorm:"size:36;not null;unique;primary_key"`
+	ParentID     string `gorm:"size:36"`
+	Section      Section
+	SectionID    string `gorm:"size:36;index"`
+	Products     []Product `gorm:many2many:product_categories`
+	Name		 string `grom:"size:100"`
+	Slug		 string `grom:"size:100"`
+	CreatedAt    time.Time
+	UpdatedAt    time.Time
+}
