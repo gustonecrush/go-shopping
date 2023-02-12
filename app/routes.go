0bf7b436e072e94bf7b1371ab835885ab98a8162
gustonecrush <codeboxezz@gmail.com>
2023-02-13 01:06:33 +0700

    user model

--- app/routes.go
+++ app/routes.go
@@ -1,7 +1,11 @@
 package app
 
-import "github.com/gustonecrush/go-shop/app/controllers"
+import (
+	"github.com/gorilla/mux"
+	"github.com/gustonecrush/go-shop/app/controllers"
+)
 
 func (server *Server) InitializeRoutes() {
+	server.Router = mux.NewRouter()
 	server.Router.HandleFunc("/", controllers.Home).Methods("GET")
 }
